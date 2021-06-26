c7aae6369c54243dbf2fb3df1836c62af9ee3f7e
Ekaterina Boikova <boykova.ekaterina.i@gmail.com>
2021-06-26 18:15:51 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const options = {
+    results: 10,
+    seed: 'abc',
+    page: 1,
+}
+
+fetch(`https://randomuser.me/api/?results=${options.result}&seed=${options.seed}&page=${options.page}`)
+    .then(response => response.json())
+    .then(({ results }) => renderUsers(results));
+
+function renderUsers(users) {
+    console.dir(users);
+}
